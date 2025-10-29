ef98a1dea3f2f90a0e61c9db67931c8b2a0507c8
Kenji Kawaida <kenjikawaidav@gmail.com>
2025-10-29 17:22:51 -0400

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 1.5.0"
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.50"
+      source  = "hashicorp/aws"
+      version = "~> 5.50"
     }
   }
 }
@@ -13,52 +13,52 @@ provider "aws" {
 }
 
 data "aws_ami" "al2023" {
-    most_recent = true
-    owners = ["137112412989"]
+  most_recent = true
+  owners      = ["137112412989"]
 
-    filter {
-      name = "name"
-      values = ["al2023-ami-*-kernel-6.1-x86_64"]
-    }
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-kernel-6.1-x86_64"]
+  }
 }
 
 resource "aws_key_pair" "this" {
-    key_name = "${var.project_name}-kp"
-    public_key = var.public_ssh_key  
+  key_name   = "${var.project_name}-kp"
+  public_key = var.public_ssh_key
 }
 
 resource "aws_security_group" "this" {
-    name = "${var.project_name}-sg"
-    description = "Accesso SSH publico"
+  name        = "${var.project_name}-sg"
+  description = "Acceso SSH limitado e HTTP publico"
 
-    ingress {
-        description = "SSH"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [var.admin_cidr_ssh]
-    }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.admin_cidr_ssh]
+  }
 
-    ingress {
-        description = "HTTP"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = [var.admin_cidr_ssh]
-    }  
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        description = "Salida libre"
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = [var.admin_cidr_ssh]
-    }
+  egress {
+    description = "Salida libre"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "${var.project_name}-sg"
-        Project = var.project_name
-    }
+  tags = {
+    Name    = "${var.project_name}-sg"
+    Project = var.project_name
+  }
 }
 
 locals {
@@ -74,15 +74,15 @@ locals {
 }
 
 resource "aws_instance" "this" {
-    ami = data.aws_ami.al2023.id
-    instance_type = "t3.micro"
-    key_name = aws_key_pair.this.key_name
-    vpc_security_group_ids = [aws_security_group.this.id]
-    user_data = local.user_data
-    associate_public_ip_address = true
+  ami                         = data.aws_ami.al2023.id
+  instance_type               = "t3.micro"
+  key_name                    = aws_key_pair.this.key_name
+  vpc_security_group_ids      = [aws_security_group.this.id]
+  user_data                   = local.user_data
+  associate_public_ip_address = true
 
-    tags = {
-      Name = "${var.project_name}-ec2"
-      Project = var.project_name
-    }
+  tags = {
+    Name    = "${var.project_name}-ec2"
+    Project = var.project_name
+  }
 }
