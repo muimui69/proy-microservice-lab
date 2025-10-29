7d4d093f574ed7e8225ce52dd3c3711a20ce1cd9
Kenji Kawaida <kenjikawaidav@gmail.com>
2025-10-29 19:22:22 -0400

    Update app.js

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (_req, res) => {
-  res.json({ status: "ok", service: "Hola Microservicio" });
+  res.json({ status: "ok", service: "Hola Microservicio 2" });
 });
 
 export default app;
